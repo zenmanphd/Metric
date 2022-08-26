5a6708cbbddaa5b385aca988bc5816eb3aebe1ce
zenmanphd <90004373+zenmanphd@users.noreply.github.com>
2022-08-26 14:44:22 -0400

    remove declaration

--- metric.js
+++ metric.js
@@ -1,5 +1,4 @@
 const header = document.querySelector(.title);
-const intro = document.querySelector(.rant);
 const go = document.querySelector(.trigger);
 go.addEventListener("click", function(){
 header.classList.toggle("change");
