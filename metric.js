9913e99880cfb2a75eb1449fc80451f6832e653e
zenmanphd <90004373+zenmanphd@users.noreply.github.com>
2022-08-26 11:32:44 -0400

    syntax correction

--- metric.js
+++ metric.js
@@ -1,6 +1,6 @@
 const header = document.querySelector(.title);
 const intro = document.querySelector(.rant);
 header.style.backgroundColor = 'red';
-header.addEventListener('click' function(){
+header.addEventListener('click', function(){
 header.style.color = 'blue';
 });
